18a95ffd548316dcccfc8d868ae96350d190f29a
Kris1027 <obarzanek.work@gmail.com>
2024-11-21 06:51:48 +0100

    connected with database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import connectDB from './config/db.js';
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, async () => {
-    console.log('Server running on port http://localhost:' + PORT);
-});
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, async () => {
+            console.log('Server running on port http://localhost:' + PORT);
+        });
+    } catch (error) {
+        console.error('Server startup error: ' + error.message);
+        process.exit(1);
+    }
+};
+
+startServer();
