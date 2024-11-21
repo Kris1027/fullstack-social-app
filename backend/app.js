f3ebaa2c5ca0b3f4af64eb4724e0979b156a3c57
Kris1027 <obarzanek.work@gmail.com>
2024-11-21 10:28:20 +0100

    add comments

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,20 @@ import express from 'express';
 import authRouter from './routes/auth.route.js';
 import startServer from './server.js';
 
+// load environment variables from a .env file into process.env
 dotenv.config();
 
+// create an instance of the express application
 export const app = express();
 
+// middleware to parse incoming JSON requests
 app.use(express.json());
+// middleware to parse URL-encoded data
 app.use(express.urlencoded({ extended: true }));
+// middleware to parse cookies in incoming requests, makes cookies available in 'req.cookies'
 app.use(cookieParser());
 
+// mount the routes
 app.use('/api/auth', authRouter);
 
 startServer();
