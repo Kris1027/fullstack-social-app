e1d952a5864dcebc8ab15cf1d892882906488892
Kris1027 <obarzanek.work@gmail.com>
2024-11-24 13:26:32 +0100

    add deletePost controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -51,3 +51,36 @@ export const createPost = async (req, res) => {
         handleControllerError('createPost', res, error);
     }
 };
+
+export const deletePost = async (req, res) => {
+    try {
+        // get the post ID from the URL parameters
+        const { id: postId } = req.params;
+
+        // get the userID from the req.user object
+        const userId = req.user._id;
+
+        // find the post in the database
+        const post = await Post.findById(postId);
+        if (!post) return res.status(404).json({ message: 'Post not found' });
+
+        // verify that the user is the owner of the post
+        if (post.user.toString() !== userId.toString()) {
+            return res.status(403).json({ message: 'You are not authorized to delete this post' });
+        }
+
+        // remove the image from cloudinary if it exists
+        if (post.image) {
+            const publicId = post.image.split('/').pop().split('.')[0]; // extract public_id from the image URL
+            await cloudinary.uploader.destroy(`fullstack-social-app/${publicId}`);
+        }
+
+        // remove the post from the database
+        await post.deleteOne();
+
+        // return a success response
+        res.status(200).json({ message: 'Post deleted successfully' });
+    } catch (error) {
+        handleControllerError('deletePost', res, error);
+    }
+};
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth-middleware.js';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, deletePost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', authMiddleware, createPost);
+router.delete('/:id', authMiddleware, deletePost);
 
 export default router;
