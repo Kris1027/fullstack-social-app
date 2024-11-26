65cb71aad7c6148f4d23d6c53487aedd8388e7a0
Kris1027 <obarzanek.work@gmail.com>
2024-11-26 14:27:41 +0100

    build createNotification function

---
+++ backend/utils/createNotification.js
@@ -0,0 +1,20 @@
+import Notification from '../models/notification.model';
+
+export const createNotification = async ({ fromUser, toUser, type, postId = null }) => {
+    try {
+        // create a new notification object
+        const notification = new Notification({
+            fromUser,
+            toUser,
+            type,
+            postId,
+        });
+
+        // save the notification in the database
+        await notification.save();
+        console.log('Notification created successfully');
+        return notification;
+    } catch (error) {
+        console.error('Error creating notifications', error);
+    }
+};
