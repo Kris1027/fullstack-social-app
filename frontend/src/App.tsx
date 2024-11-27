da6ff8bb82f4b78166001456d627ee17f6ee5c03
Kris1027 <obarzanek.work@gmail.com>
2024-11-27 09:47:09 +0100

    setup vite, tailwind and shadcnui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+function App() {
+    return (
+        <div>
+            <p className='text-red-500'>Hello</p>
+            <Button>Button</Button>
+        </div>
+    );
+}
+
+export default App;
